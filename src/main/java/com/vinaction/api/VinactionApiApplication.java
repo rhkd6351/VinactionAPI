276e6bd3769db6202e28f5b3d586327386eae16d
Lim_Yekwang <56215323+rhkd6351@users.noreply.github.com>
2021-11-22 13:53:01 +0900

    Initiate Project

---
+++ src/main/java/com/vinaction/api/VinactionApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinaction.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinactionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinactionApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinaction/api/VinactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinaction.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinactionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
